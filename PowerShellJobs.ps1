1b1e4662644b3daa2d8ad5d1c3addc3ce6905f58
olsa <ola.sannerholt@gmail.com>
2022-03-26 18:04:35 +0100

    Första incheckning

--- PowerShellJobs.ps1
+++ PowerShellJobs.ps1
@@ -1,3 +1,26 @@
 ﻿#
 # Script.ps1
 #
+Clear-Host
+Start-Job {1..10 | foreach {$_ ; Start-Sleep -Seconds 1}} -Name J1
+Start-Job {1..10 | foreach {$_ ; Start-Sleep -Seconds 2}} -Name J2
+
+$nc = $true
+while ($nc)
+{
+    $nc = $false
+    Get-Job -State Completed
+    $js = Get-Job
+    foreach ($j in $js)
+    {
+       if ($j.State -ne "Completed")
+       {
+          Write-Host "$($j.Name) Job Not Completed"
+          $nc = $true
+       }
+    }
+    Start-Sleep -Seconds 5
+}
+#$js | gm
+#Get-Job | Remove-Job
+
